2c205dc6d4051ebb997b9731184fbd86fc681206
Vladimir198401 <varonin54@gmail.com>
2019-12-13 12:40:30 +0200

    sutvarkyta

--- Automobiliai/masinos.class.js
+++ Automobiliai/masinos.class.js
@@ -4,43 +4,21 @@
 		constructor(masina) {  //parametrai
  
 		 	// parametrai vadinami savibemis ir priskiriama this
+			this.Automobilis = masina.Automobilis
 			this.kuro_san_mieste = masina.kuro_san_mieste
 			this.kuro_san_uzmiestij = masina.kuro_san_uzmiestij
+			
 		}
 		
-		present() {  			//metodai - taip vadinamos funkcijos priklausancios klasei
+		present( san_proc_mieste ) {  			//metodai - taip vadinamos funkcijos priklausancios klasei
 							// metodai dirba su savybemis per kreipini this. savybes bendros visems metodams 
 			
-			return ' - kuro_san_uzmiestij: ' + this.kuro_san_uzmiestij + ' - kuro_san_mieste: ' +  this.kuro_san_mieste +  ' - vidurkis:' + this.vidurkis();    //return apraso ka turi grazint ir kaip veikt ir parodo front ende
+			return ' - Automobilis: ' + this.Automobilis + ' - kuro_san_uzmiestij: ' + this.kuro_san_uzmiestij + ' - kuro_san_mieste: ' +  this.kuro_san_mieste +  ' - vidurkis:' + this.vidurkis( san_proc_mieste );    //return apraso ka turi grazint ir kaip veikt ir parodo front ende
 		}
 		
-		vidurkis() {
+		vidurkis( san_proc_mieste) {
 		
-			return (this.kuro_san_mieste + this.kuro_san_uzmiestij) / 2;  //  return this sujungia padauginant visus matmenis, suskaiciojant turi
-			//(this.kuro_san_mieste ) = 30% + (this.kuro_san_uzmiestij) = 100%; 
+			return this.kuro_san_mieste * sanaudu_proc_mieste / 100 + ( this.kuro_san_uzmiestij * (100 - sanaudu_proc_mieste ) / 100 );
 		}
-		
-		arYraRibose( sanaudos_nuo, sanaudos_iki, kuro_san_uzmiestij, kuro_san_mieste) {		//paimamos reiksmes
 
-			var ar_yraRybose = false;   // kintamajam ar_tilps suteikiama klaidos reiksme to atveju jai ar tilps neatitinka dotus parametrus
-			
-			if (((sanaudos_nuo <= this.kuro_san_uzmiestij) 
-			
-				||
-					(sanaudos_nuo <=  this.vidurkis())) 
-				||
-					(sanaudos_nuo >= this.vidurkis)
-				||
-					( sanaudos_iki <= this.vidurkis())) { 
-						
-						// paimamos reiksmes
-				//suteikiama parametru pozicija nustatitose reiksmese 
-				// bus kartojama keleta kartu keiciant reiksmiu pozicijas.
-		
-					ar_yraRybose = true;	// kintamajam ar_tilps suteikiama tesos reiksme to atveju jai ar_tilps atitinka dotus parametrus
-			
-			}
-			
-			return ar_yraRybose; 			// uzdaromas ciklas
-		}
-	} 
+	}	
--- Automobiliai/masinu_sarasas.js
+++ Automobiliai/masinu_sarasas.js
@@ -1,10 +1,8 @@
-	
 	var lst_masina = [
 	
-		{ pav: 'Suzuki' , kuro_san_mieste: 8.50 , kuro_san_uzmiestij: 5.6  } ,
-		{ pav: 'Audi' , kuro_san_mieste: 10.00  , kuro_san_uzmiestij: 5.7  } ,
-		{ pav: 'Honda' , kuro_san_mieste: 7.00  , kuro_san_uzmiestij: 4.5  } ,
-		{ pav: 'VV' , kuro_san_mieste: 6.50  , kuro_san_uzmiestij: 5.0  } ,
-		{ pav: 'BMW' , kuro_san_mieste: 12.00  , kuro_san_uzmiestij: 8.0 } ,
-		{ pav: 'Hiundai' , kuro_san_mieste: 4.50  , kuro_san_uzmiestij: 3.5  }
+		{ Automobilis: 'Suzuki' , kuro_san_mieste: 8.50 , kuro_san_uzmiestij: 5.6  } ,
+		{ Automobilis: 'Audi' , kuro_san_mieste: 10.00  , kuro_san_uzmiestij: 5.7  } ,
+		{ Automobilis: 'Honda' , kuro_san_mieste: 7.00  , kuro_san_uzmiestij: 4.5  } ,
+		{ Automobilis: 'VV' , kuro_san_mieste: 6.50  , kuro_san_uzmiestij: 5.0  } ,
+		{ Automobilis: 'BMW' , kuro_san_mieste: 12.00  , kuro_san_uzmiestij: 8.0 } ,
 	]; 
